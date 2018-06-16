164d739cf7741570eec29c9c785f28cb7429898f
Marijana Presečki <mpresecki@foi.hr>
2018-06-16 22:28:05 +0200

    Dodaj sloj poslovne logike i klasu NavodnjavanjeUsluge
    Dodan je projekt u kojem će se nalaziti poslovna logika. Također je dodana statička klasa NavodnjavanjeUsluge pomoću koje će forme/kontrole dohvaćati zapise o navodnjavanju i te iste mijenjati.

---
+++ PoljoApp/BusinessLayer/NavodnjavanjeUsluge.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer;
+using PoljoAppModel;
+
+namespace BusinessLayer
+{
+    public static class NavodnjavanjeUsluge
+    {
+        static INavodnjavanjeRepozitorij Repozitorij;
+
+        static NavodnjavanjeUsluge()
+        {
+            Repozitorij = new NavodnjavanjeRepozitorij();
+        }
+
+        public static List<NavodnjavanjeView> DohvatiSve(int godina)
+        {
+            return Repozitorij.DohvatiNavodnjavanje(godina);
+        }
+
+        public static List<NavodnjavanjeView> DohvatiOborine()
+        {
+            return Repozitorij.DohvatiOborine();
+        }
+
+        public static Navodnjavanje DohvatiPoIdu(int id)
+        {
+            return Repozitorij.DohvatiPoIdu(id);
+        }
+
+        public static Navodnjavanje Spremi(Navodnjavanje novo)
+        {
+            return Repozitorij.Spremi(novo);
+        }
+
+        public static void Azuriraj(Navodnjavanje zaIzmjenu)
+        {
+            Repozitorij.Azuriraj(zaIzmjenu);
+        }
+
+        public static void Izbrisi(Navodnjavanje zaBrisanje)
+        {
+            Repozitorij.Izbrisi(zaBrisanje);
+        }
+
+        public static void OdbijOborinu(Navodnjavanje zaIzmjenu)
+        {
+            Repozitorij.OdbijOborinu(zaIzmjenu);
+        }
+    }
+}
