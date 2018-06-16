f40727441e94f9bfed12cc1161131e16514cb0c5
Marijana Presečki <mpresecki@foi.hr>
2018-06-16 21:32:15 +0200

    Dodaj logiku pristupa podacima o prskanju
    Dodano je sučelje te klasa koja implementira to sučelje, a omogućuje rukovanje podacima o prskanju iz baze. To se odnosi na dohvaćanje, ažuriranje, spremanje te brisanje iz baze.

---
+++ PoljoApp/DataLayer/IPrskanjeRepozitorij.cs
@@ -0,0 +1,22 @@
+﻿using PoljoAppModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public interface IPrskanjeRepozitorij
+    {
+        List<PrskanjeView> DohvatiPrskanje();
+
+        Prskanje DohvatiPoIdu(int id);
+
+        Prskanje Spremi(Prskanje novo);
+
+        void Azuriraj(Prskanje zaIzmjenu);
+
+        void Izbrisi(Prskanje zaBrisanje);
+    }
+}
---
+++ PoljoApp/DataLayer/PrskanjeRepozitorij.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PoljoAppModel;
+
+namespace DataLayer
+{
+    public class PrskanjeRepozitorij : IPrskanjeRepozitorij
+    {
+        public void Azuriraj(Prskanje zaIzmjenu)
+        {
+            using (var db = new Entities())
+            {
+                db.Prskanje.Attach(zaIzmjenu);
+                db.SaveChanges();
+            }
+        }
+
+        public Prskanje DohvatiPoIdu(int id)
+        {
+            using (var db = new Entities())
+            {
+                return db.Prskanje.Find(id);
+            }
+        }
+
+        public List<PrskanjeView> DohvatiPrskanje()
+        {
+            using (var db = new Entities())
+            {
+                return db.PrskanjeView.ToList();
+            }
+        }
+
+        public void Izbrisi(Prskanje zaBrisanje)
+        {
+            using (var db = new Entities())
+            {
+                db.Prskanje.Attach(zaBrisanje);
+                db.Prskanje.Remove(zaBrisanje);
+                db.SaveChanges();
+            }
+        }
+
+        public Prskanje Spremi(Prskanje novo)
+        {
+            using (var db = new Entities())
+            {
+                db.Prskanje.Add(novo);
+                db.SaveChanges();
+                return novo;
+            }
+        }
+    }
+}
