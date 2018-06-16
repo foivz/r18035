68a8f72230bb2634ec7b7ec6f8f889a8752de6c9
Marijana Presečki <mpresecki@foi.hr>
2018-06-16 21:31:42 +0200

    Dodaj logiku pristupa podacima o navodnjavanju
    Dodano je sučelje te klasa koja implementira to sučelje, a omogućuje rukovanje podacima o navodnjavanju iz baze. To se odnosi na dohvaćanje, ažuriranje, spremanje te brisanje iz baze.

---
+++ PoljoApp/DataLayer/INavodnjavanjeRepozitorij.cs
@@ -0,0 +1,22 @@
+﻿using PoljoAppModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public interface INavodnjavanjeRepozitorij
+    {
+        List<NavodnjavanjeView> DohvatiNavodnjavanje();
+
+        Navodnjavanje DohvatiPoIdu(int id);
+
+        Navodnjavanje Spremi(Navodnjavanje novo);
+
+        void Azuriraj(Navodnjavanje zaIzmjenu);
+
+        void Izbrisi(Navodnjavanje zaBrisanje);
+    }
+}
---
+++ PoljoApp/DataLayer/NavodnjavanjeRepozitorij.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PoljoAppModel;
+
+namespace DataLayer
+{
+    public class NavodnjavanjeRepozitorij : INavodnjavanjeRepozitorij
+    {
+        public void Azuriraj(Navodnjavanje zaIzmjenu)
+        {
+            using (var db = new Entities())
+            {
+                db.Navodnjavanje.Attach(zaIzmjenu);
+                db.SaveChanges();
+            }
+        }
+
+        public List<NavodnjavanjeView> DohvatiNavodnjavanje()
+        {
+            using (var db = new Entities())
+            {
+                return db.NavodnjavanjeView.ToList();
+            }
+        }
+
+        public Navodnjavanje DohvatiPoIdu(int id)
+        {
+            using (var db = new Entities())
+            {
+                return db.Navodnjavanje.Find(id);
+            }
+        }
+
+        public void Izbrisi(Navodnjavanje zaBrisanje)
+        {
+            using (var db = new Entities())
+            {
+                db.Navodnjavanje.Attach(zaBrisanje);
+                db.Navodnjavanje.Remove(zaBrisanje);
+                db.SaveChanges();
+            }
+        }
+
+        public Navodnjavanje Spremi(Navodnjavanje novo)
+        {
+            using (var db = new Entities())
+            {
+                db.Navodnjavanje.Add(novo);
+                db.SaveChanges();
+                return novo;
+            }
+        }
+    }
+}
