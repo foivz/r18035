a052692d7741422d1749c1d9199a4758f35f1bd1
Marijana Presečki <mpresecki@foi.hr>
2018-06-17 20:10:42 +0200

    Dodaj klase DjelatniciUsluge i PrskanjeUsluge
    Dodane su klase u sloj poslovne logike koje služe za komunikaciju s podatkovnim slojem, a omogućuju dohvaćenje i manipuliranje podacima iz istoimenih funkcionalnosti.

---
+++ PoljoApp/BusinessLayer/DjelatniciUsluge.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PoljoAppModel;
+using DataLayer;
+
+namespace BusinessLayer
+{
+    public static class DjelatniciUsluge
+    {
+        static IDjelatniciRepozitorij Repozitorij;
+
+        static DjelatniciUsluge()
+        {
+            Repozitorij = new DjelatniciRepozitorij();
+        }
+
+        public static List<Djelatnik> DohvatiSve()
+        {
+            return Repozitorij.DohvatiSve();
+        }
+
+        public static Djelatnik DohvatiPoIdu(int id)
+        {
+            return Repozitorij.DohvatiPoIdu(id);
+        }
+
+        public static Djelatnik Spremi(Djelatnik novo)
+        {
+            return Repozitorij.Spremi(novo);
+        }
+
+        public static void Azuriraj(Djelatnik zaIzmjenu)
+        {
+            Repozitorij.Azuriraj(zaIzmjenu);
+        }
+
+        public static void Izbrisi(Djelatnik zaBrisanje)
+        {
+            Repozitorij.Izbrisi(zaBrisanje);
+        }
+    }
+}
---
+++ PoljoApp/BusinessLayer/PrskanjeUsluge.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PoljoAppModel;
+using DataLayer;
+
+namespace BusinessLayer
+{
+    public static class PrskanjeUsluge
+    {
+        static IPrskanjeRepozitorij Repozitorij;
+
+        static PrskanjeUsluge()
+        {
+            Repozitorij = new PrskanjeRepozitorij();
+        }
+
+        public static List<PrskanjeView> DohvatiSve(int godina)
+        {
+            return Repozitorij.DohvatiPrskanje(godina);
+        }
+
+        public static Prskanje DohvatiPoIdu(int id)
+        {
+            return Repozitorij.DohvatiPoIdu(id);
+        }
+
+        public static Prskanje Spremi(Prskanje novo)
+        {
+            return Repozitorij.Spremi(novo);
+        }
+
+        public static void Azuriraj (Prskanje zaIzmjenu)
+        {
+            Repozitorij.Azuriraj(zaIzmjenu);
+        }
+
+        public static void Izbrisi(Prskanje zaBrisanje)
+        {
+            Repozitorij.Izbrisi(zaBrisanje);
+        }
+
+        public static List<int> DohvatiGodine()
+        {
+            return Repozitorij.DohvatiGodine();
+        }
+    }
+}
