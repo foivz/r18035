a77f0e3f6920a62f91e9572278b13ea212fbeb7d
Marijana Presečki <mpresecki@foi.hr>
2018-06-17 20:07:33 +0200

    Dodaj klasu za pristupanje podacima o djelatnicima
    Dodano je sučelje i klasa koja omogućuje pristup i rukovanje podacima o djelatnicima.

---
+++ PoljoApp/DataLayer/DjelatniciRepozitorij.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PoljoAppModel;
+
+namespace DataLayer
+{
+    public class DjelatniciRepozitorij : IDjelatniciRepozitorij
+    {
+        public void Azuriraj(Djelatnik zaIzmjenu)
+        {
+            using (var db = new Entities())
+            {
+                Djelatnik mijenjaj = db.Djelatnik.Find(zaIzmjenu.Id);
+                mijenjaj.Ime = zaIzmjenu.Ime;
+                mijenjaj.Prezime = zaIzmjenu.Prezime;
+                mijenjaj.Lozinka = zaIzmjenu.Lozinka;
+                mijenjaj.Email = zaIzmjenu.Email;
+                mijenjaj.BrojTelefona = zaIzmjenu.BrojTelefona;
+                db.SaveChanges();
+            }
+        }
+
+        public Djelatnik DohvatiPoIdu(int id)
+        {
+            using (var db = new Entities())
+            {
+                return db.Djelatnik.Find(id);
+            }
+        }
+
+        public List<Djelatnik> DohvatiSve()
+        {
+            using (var db = new Entities())
+            {
+                return db.Djelatnik.ToList();
+            }
+        }
+
+        public void Izbrisi(Djelatnik zaBrisanje)
+        {
+            using (var db = new Entities())
+            {
+                db.Djelatnik.Attach(zaBrisanje);
+                db.Djelatnik.Remove(zaBrisanje);
+                db.SaveChanges();
+            }
+        }
+
+        public Djelatnik Spremi(Djelatnik novo)
+        {
+            using (var db = new Entities())
+            {
+                db.Djelatnik.Add(novo);
+                db.SaveChanges();
+                return novo;
+            }
+        }
+    }
+}
---
+++ PoljoApp/DataLayer/IDjelatniciRepozitorij.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PoljoAppModel;
+
+namespace DataLayer
+{
+    public interface IDjelatniciRepozitorij
+    {
+        List<Djelatnik> DohvatiSve();
+
+        Djelatnik DohvatiPoIdu(int id);
+
+        Djelatnik Spremi(Djelatnik novo);
+
+        void Azuriraj(Djelatnik zaIzmjenu);
+
+        void Izbrisi(Djelatnik zaBrisanje);
+    }
+}
